1d273dff3356c6ad0c7b87422e4c5a105e0c31e3
vijay babar <babarvm@gmail.com>
2024-02-03 10:07:04 +0530

    MVC in Action

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id=1, Name="Shrek" };
+
+            return View(movie);
+        }
+    }
+}
