2ac2244a3ae1c7ca527d61e0dfb6287141bb4a54
vijay babar <babarvm@gmail.com>
2024-02-03 14:54:41 +0530

    ViewData, ViewBag and TempData

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,36 +10,11 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Id=1, Name="Shrek" };
 
-            //return View(movie);
-            //return Content("Hello world");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
-        }
+            ViewData["Movie"] = movie;
+            ViewBag.Movie = movie;
+            TempData["Movie"] = movie;
 
-        // GET: Movies/Edit/Id
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        // GET: Movies/Index
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "name";
-
-            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        // GET: movies/released/{year}/{month}
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
+            return View();
         }
     }
 }
