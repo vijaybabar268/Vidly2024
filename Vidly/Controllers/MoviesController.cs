45dd78ca87c57410d7fe97a49fa3d31d5f01097d
vijay babar <babarvm@gmail.com>
2024-02-03 15:14:46 +0530

    ViewModels and Razor syntax

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -9,12 +11,23 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Id=1, Name="Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name= "Customer 1" },
+                new Customer { Id = 2, Name= "Customer 2"},
+                new Customer { Id = 3, Name= "Customer 3" },
+                new Customer { Id = 4, Name= "Customer 4"},
+                new Customer { Id = 5, Name= "Customer 5" },
+                new Customer { Id = 6, Name= "Customer 6"}
+            };
 
-            ViewData["Movie"] = movie;
-            ViewBag.Movie = movie;
-            TempData["Movie"] = movie;
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
 
-            return View();
+            return View(viewModel);
         }
     }
 }
