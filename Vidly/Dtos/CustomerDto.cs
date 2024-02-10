8a8b25504ac250d5257ad800389987942e28f6dd
vijay babar <babarvm@gmail.com>
2024-02-10 19:16:18 +0530

    Build an API, Added DTO and Make use of AutoMapper tool

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }               
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
