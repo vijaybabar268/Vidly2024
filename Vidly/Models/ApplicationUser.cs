5b172f5c8d21a2433c41df13ddfe6727371f02e6
vijay babar <babarvm@gmail.com>
2024-03-02 22:29:27 +0530

    End of the Section 8: Authentication and Authorization

--- Vidly/Models/ApplicationUser.cs
+++ Vidly/Models/ApplicationUser.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -21,23 +20,5 @@ namespace Vidly.Models
             // Add custom user claims here
             return userIdentity;
         }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<Genre> Genres { get; set; }
     }
 }
