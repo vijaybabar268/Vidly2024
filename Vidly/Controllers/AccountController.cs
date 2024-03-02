d14199464e66d3290f8d7e2b225c6310a3cda608
vijay babar <babarvm@gmail.com>
2024-03-02 21:11:23 +0530

    Added user profile data

--- Vidly/Controllers/AccountController.cs
+++ Vidly/Controllers/AccountController.cs
@@ -153,8 +153,9 @@ namespace Vidly.Controllers
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { 
-                    UserName = model.Email, 
-                    Email = model.Email 
+                    UserName = model.Email,                     
+                    Email = model.Email,
+                    DrivingLicense = model.DrivingLicense
                 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
